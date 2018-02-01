05a45ea9f656a9389e0a835d349312f991d1914a
Gbenga Oshinaga <andeladeveloper@Andelas-MacBook-Pro.local>
2018-02-01 23:51:04 +0100

    feature(user-onboarding): add onboarding for dashboard [#154621251]

--- imports/plugins/included/product-variant/components/productGrid.js
+++ imports/plugins/included/product-variant/components/productGrid.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Reaction } from "/client/api";
 import { Components } from "@reactioncommerce/reaction-components";
 import startTour from "../../tour";
 
@@ -10,9 +11,15 @@ class ProductGrid extends Component {
 
   componentDidMount() {
     const hasTakenLandingPageTour = localStorage.getItem("hasTakenLandingPageTour");
+    const hasTakenDashboardTour = localStorage.getItem("hasTakenDashboardTour");
+
     if (!hasTakenLandingPageTour) {
       startTour();
       localStorage.setItem("hasTakenLandingPageTour", true);
+    }
+    if (!hasTakenDashboardTour && Reaction.hasPermission("admin")) {
+      startTour();
+      localStorage.setItem("hasTakenDashboardTour", true);
     }
   }
 
--- imports/plugins/included/tour/index.js
+++ imports/plugins/included/tour/index.js
@@ -1,11 +1,19 @@
 import { introJs } from "intro.js";
+import { Reaction } from "/client/api";
 import landingPageTour from "./landingPageTour";
+import dashboardTour from "./dashboardTour";
 
 const intro = new introJs();
 
 const startTour = () => {
+  let steps;
+
+  Reaction.hasPermission("admin") ?
+    steps = dashboardTour :
+    steps = landingPageTour;
+
   intro.setOptions({
-    steps: landingPageTour,
+    steps,
     showBullets: true,
     showProgress: true,
     scrollToElement: true,
