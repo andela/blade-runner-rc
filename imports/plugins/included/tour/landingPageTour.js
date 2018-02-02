2a1ef01cd23cc2f81b51bfa37b27e05bbf0c4a2c
Gbenga Oshinaga <andeladeveloper@Andelas-MacBook-Pro.local>
2018-02-02 11:02:35 +0100

    feature(user-onboarding): make intro text more descriptive [Finishes #154621251]

--- imports/plugins/included/tour/landingPageTour.js
+++ imports/plugins/included/tour/landingPageTour.js
@@ -2,43 +2,45 @@ const landingPageTour = [
   {
     intro: `<h3>Welcome to ReactionCommerce</h3>
             <hr>
+            <p>Hello there! Welcome to ReactionCommerce, a place where you can find all kinds of 
+            product to suit your every want and need.</p>
             <p>Let me take you on brief tour</p>`
   },
   {
     element: ".cart-container",
     intro: `<h3>Shopping Cart</h3>
             <hr>
-            <p>You can find your orders here</p>`
+            <p>You can find all products you selected for purchase here. Click here to proceed to checkout</p>`
   },
   {
     element: ".accounts",
     intro: `<h3>Accounts</h3>
             <hr>
-            <p>Come here to register for an account</p>`
+            <p>Come here to register for an account, sign in or sign out</p>`
   },
   {
     element: ".currencies",
     intro: `<h3>Currencies</h3>
             <hr>
-            <p>Select the currency that suits you here</p>`
+            <p>Select the currency that suits you here for making payment</p>`
   },
   {
     element: ".languages",
     intro: `<h3></h3>
             <hr>
-            <p>Select language here</p>`
+            <p>Select your preferred language here</p>`
   },
   {
     element: ".search",
     intro: `<h3>Search</h3>
             <hr>
-            <p>Search for products here</p>`
+            <p>Search for specific products here with our real time search</p>`
   },
   {
     element: ".product-grid",
     intro: `<h3>Products</h3>
             <hr>
-            <p>Select product to purchase and add to your cart here</p>`
+            <p>Browse through our gallery of products and select one to purchase and add to your cart</p>`
   }
 ];
 
