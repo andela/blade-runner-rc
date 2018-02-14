cd3e7352b0dca5043e198080843e4eb3109dea83
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-02-14 07:44:37 +0100

    feature(social contents) facebook-content
    integrate facebook contents
    
    [Finishes #154621257]

--- imports/plugins/included/product-detail-simple/client/components/addToCartButton.js
+++ imports/plugins/included/product-detail-simple/client/components/addToCartButton.js
@@ -16,26 +16,37 @@ class AddToCartButton extends Component {
   render() {
     if (this.hasVariants) {
       return (
-        <div className="pdp add-to-cart block">
-          <input
-            className="form-control input-md"
-            id="add-to-cart-quantity"
-            min="1"
-            name="addToCartQty"
-            onChange={this.handleCartQuantityChange}
-            type="number"
-            value={this.props.cartQuantity}
-          />
-          <button
-            className="input-group-addon add-to-cart-text js-add-to-cart"
-            data-i18n="productDetail.addToCart"
-            onClick={this.props.onClick || this.props.onAddToCart}
-          >
-            <Components.Translation defaultValue="Add to cart" i18nKey="productDetail.addToCart" />
-          </button>
+        <div>
+          <div className="pdp add-to-cart block">
+            <input
+              className="form-control input-md"
+              id="add-to-cart-quantity"
+              min="1"
+              name="addToCartQty"
+              onChange={this.handleCartQuantityChange}
+              type="number"
+              value={this.props.cartQuantity}
+            />
+            <button
+              className="input-group-addon add-to-cart-text js-add-to-cart"
+              data-i18n="productDetail.addToCart"
+              onClick={this.props.onClick || this.props.onAddToCart}
+            >
+              <Components.Translation defaultValue="Add to cart" i18nKey="productDetail.addToCart" />
+            </button>
+          </div>
+
+          <div>
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBlade-Runner-RC-156938035101974%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1370390833065544" width="340" height="500" style={{ border: "none", overflow: "hidden", marginTop: "10rem" }}
+              scrolling="no" frameBorder="0" allowTransparency="true"
+            />
+          </div>
+          <div>
+          </div>
         </div>
       );
     }
+
 
     if (this.props.editable && this.hasVariants === false) {
       return (
