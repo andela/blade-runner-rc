ebc1ede35b702443ccf9340ef6e1e415f4c19035
Sanni Kehinde <kensanni@gmail.com>
2018-02-16 12:58:36 +0100

    feature(UI): add digital product section [#154621249]

--- imports/plugins/included/ui-home/client/index.js
+++ imports/plugins/included/ui-home/client/index.js
@@ -2,3 +2,4 @@ import "./templates/home.html";
 import "./templates/featuredProducts.html";
 import "./templates/category.html";
 import "./templates/category.js";
+import "./templates/digitalProducts.html";
